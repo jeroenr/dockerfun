55d4822f5dd454fd86ea208edba9be9a34bb058e
Jeroen Rosenberg <jeroen.rosenberg@ibanx-hse.com>
2015-03-25 14:03:20 +0100

    Fixed script

--- run_stack.sh
+++ run_stack.sh
@@ -9,7 +9,7 @@ MYSQL_ID=$(docker run -d --volumes-from data11 -e STARTUP_SQL=/tmp/create_unite_
 RUN_ID=`date -u +%s`
 
 # Start tomcat
-TOMCAT_ID=$(docker run -d --volumes-from data11 --volumes-from consul7 -p 8080 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} --link $MYSQL_ID:mysql ibanx/tomcat)
+TOMCAT_ID=$(docker run -d --volumes-from data11 --volumes-from consul7 -p 8080 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} --link $MYSQL_ID:unitedb --link $MYSQL_ID:jackrabbitdb ibanx/tomcat)
 
 # Start nginx
 NGINX_ID=$(docker run -d --volumes-from data11 --volumes-from consul7 -p 80 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} ibanx/nginx)
