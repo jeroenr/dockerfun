95c9d000d83b5596425bc0ff69ab9f917baa0b45
Jeroen Rosenberg <jeroen.rosenberg@ibanx-hse.com>
2015-03-25 13:56:03 +0100

    Elasticsearch data volume and run script

---
+++ run_stack.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -x
+
+# Run the database
+MYSQL_ID=$(docker run -d --volumes-from data11 -e STARTUP_SQL=/tmp/create_unite_dbs.sql ibanx/mysql)
+
+# Create a RUN_ID
+RUN_ID=`date -u +%s`
+
+# Start tomcat
+TOMCAT_ID=$(docker run -d --volumes-from data11 --volumes-from consul7 -p 8080 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} --link $MYSQL_ID:mysql ibanx/tomcat)
+
+# Start nginx
+NGINX_ID=$(docker run -d --volumes-from data11 --volumes-from consul7 -p 80 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} ibanx/nginx)
+
+
