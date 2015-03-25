9ad3dc030ddc5bef9f5659e4361c5ac9dd2cb3d1
Jeroen Rosenberg <jeroen.rosenberg@ibanx-hse.com>
2015-03-25 10:52:18 +0100

    Fix run consul script

--- images/tomcat/run.sh
+++ images/tomcat/run.sh
@@ -1,5 +1,5 @@
 echo Running consul-template
 
-consul-template -once -retry 10s -config "/home/user/consul/consul.conf"
+consul-template -once -retry 10s -config "/consul/tomcat/consul.conf"
 
 tail -F /home/user/unite/logs/*.log
