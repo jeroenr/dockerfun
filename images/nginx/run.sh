6f6fc12cbd2732d217dde17bfe0e866a235468e9
Jeroen Rosenberg <jeroen.rosenberg@ibanx-hse.com>
2015-03-25 09:39:15 +0100

    Configure nginx using consul

--- images/nginx/run.sh
+++ images/nginx/run.sh
@@ -1,5 +1,5 @@
 echo Running consul-template
 
-consul-template -once -retry 10s -config "/home/user/consul/consul.conf"
+consul-template -once -retry 10s -config "/consul/nginx/consul.conf"
 
 tail -F /var/log/nginx/*
