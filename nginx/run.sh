87f485e70b90f97966ce0b9cd588bf9c73bf8ec3
Jeroen Rosenberg <jeroen.rosenberg@ibanx-hse.com>
2015-03-30 17:18:44 +0200

    I think this setup works, except you need substantial resources (RAM)

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,5 @@
 echo Running consul-template
 
-consul-template -once -retry 10s -config "/consul/nginx/consul.conf"
+consul-template -log-level debug -config "/consul/nginx/consul.conf"
 
 tail -F /var/log/nginx/*
--- run_stack.sh
+++ run_stack.sh
@@ -2,21 +2,24 @@
 
 set -x
 
-# Run the database
-MYSQL_ID=$(docker run -d --volumes-from data11 -e STARTUP_SQL=/tmp/create_unite_dbs.sql ibanx/mysql)
-
 # Run volumes
 CONSUL_ID=$(docker create ibanx/consul)
 
 DATA_ID=$(docker create ibanx/data)
 
+# Run the database
+MYSQL_ID=$(docker run -d --volumes-from ${DATA_ID} -e STARTUP_SQL=/tmp/create_unite_dbs.sql ibanx/mysql)
+
 # Create a RUN_ID
 RUN_ID=`date -u +%s`
+
+# Start nginx
+NGINX_ID=$(docker run -d --volumes-from ${DATA_ID} --volumes-from ${CONSUL_ID} -p 80 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} ibanx/nginx)
+
+sleep 2
 
 # Start tomcat
 TOMCAT_ID=$(docker run -d --volumes-from ${DATA_ID} --volumes-from ${CONSUL_ID} -p 8080 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} --link $MYSQL_ID:unitedb --link $MYSQL_ID:jackrabbitdb ibanx/tomcat)
 
-# Start nginx
-NGINX_ID=$(docker run -d --volumes-from ${DATA_ID} --volumes-from ${CONSUL_ID} -p 80 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} ibanx/nginx)
 
 
