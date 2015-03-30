0e36e781927ec92942c86b1db238b44574fee3d1
Jeroen Rosenberg <jeroen.rosenberg@ibanx-hse.com>
2015-03-30 13:48:18 +0200

    Script to run service registry

---
+++ run_service_registry.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -x
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+export HOSTNAME=$1
+export HOST_ADDR=$2
+
+docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h $HOSTNAME progrium/consul -server -bootstrap -ui-dir /ui
+docker run -d -v /var/run/docker.sock:/tmp/docker.sock -h $HOSTNAME gliderlabs/registrator:master -ip $HOST_ADDR consul://$HOST_ADDR:8500
+
--- run_stack.sh
+++ run_stack.sh
@@ -5,13 +5,18 @@ set -x
 # Run the database
 MYSQL_ID=$(docker run -d --volumes-from data11 -e STARTUP_SQL=/tmp/create_unite_dbs.sql ibanx/mysql)
 
+# Run volumes
+CONSUL_ID=$(docker create ibanx/consul)
+
+DATA_ID=$(docker create ibanx/data)
+
 # Create a RUN_ID
 RUN_ID=`date -u +%s`
 
 # Start tomcat
-TOMCAT_ID=$(docker run -d --volumes-from data11 --volumes-from consul7 -p 8080 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} --link $MYSQL_ID:unitedb --link $MYSQL_ID:jackrabbitdb ibanx/tomcat)
+TOMCAT_ID=$(docker run -d --volumes-from ${DATA_ID} --volumes-from ${CONSUL_ID} -p 8080 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} --link $MYSQL_ID:unitedb --link $MYSQL_ID:jackrabbitdb ibanx/tomcat)
 
 # Start nginx
-NGINX_ID=$(docker run -d --volumes-from data11 --volumes-from consul7 -p 80 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} ibanx/nginx)
+NGINX_ID=$(docker run -d --volumes-from ${DATA_ID} --volumes-from ${CONSUL_ID} -p 80 -e SERVICE_TAGS=${RUN_ID} -e TAG=${RUN_ID} ibanx/nginx)
 
 
